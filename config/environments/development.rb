dccaeabe491ddd1972d4545b3f71ec1ade0ddcdc
Makenzie Schwartz <schwartz.makenzie@gmail.com>
2014-03-05 16:47:21 -0500

    IDK but things were changed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,19 @@ Sab::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+
+config.action_mailer.smtp_settings = {
+address: "smtp.gmail.com",
+port: 587,
+domain:"sabweekender.com",
+authentication: "plain",
+enable_starttls_auto: true,
+user_name:"comp630@gmail.com",
+password:"rubyonrails"
+}
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
